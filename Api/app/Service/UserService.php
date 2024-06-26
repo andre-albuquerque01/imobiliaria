a81668178b028866e0853123f2d1b803d9df4d81
andre <andrealbuquerque2001@gmail.com>
2024-06-26 12:12:51 -0300

    feat: create new functions

--- Api/app/Service/UserService.php
+++ Api/app/Service/UserService.php
@@ -6,10 +6,15 @@ use App\Exceptions\UserException;
 use App\Http\Resources\AuthResource;
 use App\Http\Resources\GeneralResource;
 use App\Http\Resources\UserResource;
+use App\Jobs\SendRecoverPasswordEmailJob;
+use App\Jobs\SendVerifyEmailJob;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 
 class UserService
 {
@@ -46,7 +51,9 @@ class UserService
         try {
             $data['password'] = Hash::make($data['password']);
             $data['is_admin'] = 0;
-            User::create($data);
+            $data['remember_token'] = Str::random(60);
+            $user = User::create($data);
+            dispatch(new SendVerifyEmailJob($user->email, $user->remember_token, $user->idUser));
             return new GeneralResource(['message' => 'success']);
         } catch (\Exception $e) {
             throw new UserException('', $e->getCode(), $e);
@@ -80,6 +87,75 @@ class UserService
         try {
             User::findOrFail(auth()->user()->idUser)->delete();
             return new GeneralResource(['message' => 'success']);
+        } catch (\Exception $e) {
+            throw new UserException('', $e->getCode(), $e);
+        }
+    }
+
+    public function resendEmail(string $email)
+    {
+        try {
+            $user = User::where('email', $email)->first();
+            if (!$user) {
+                throw new UserException('', 400);
+            }
+            dispatch(new SendVerifyEmailJob($user->email, $user->remember_token, $user->idUser));
+            return new GeneralResource(['message' => 'success']);
+        } catch (\Exception $e) {
+            throw new UserException('', $e->getCode(), $e);
+        }
+    }
+    public function validateEmail(string $id, string $token)
+    {
+        try {
+            $user = User::where('idUser', $id)->where("remember_token", $token)->first();
+            if (!$user) throw new UserException('', 400);
+
+            $user->touch('email_verified_at');
+            return new GeneralResource(['message' => 'success']);
+        } catch (\Exception $e) {
+            throw new UserException('', $e->getCode(), $e);
+        }
+    }
+
+    public function recoverPasswordSendEmail(string $email)
+    {
+        try {
+            $user = User::where("email", $email)->first();
+            if (!$user) throw new UserException("User not found");
+
+            $token = Str::random(60);
+            $passwordResetToken = DB::table('password_reset_tokens')->where('email', $email)->first();
+
+            if ($passwordResetToken) {
+                DB::table('password_reset_tokens')->where('email', $email)->update([
+                    'token' => $token,
+                    'created_at' => now(),
+                ]);
+            } else {
+                DB::table('password_reset_tokens')->insert([
+                    'email' => $user->email,
+                    'token' => $token,
+                    'created_at' => now(),
+                ]);
+            }
+
+            SendRecoverPasswordEmailJob::dispatch($email, $token);
+            return new GeneralResource(['message' => 'success']);
+        } catch (\Exception $e) {
+            throw new UserException('', $e->getCode(), $e);
+        }
+    }
+
+    public function resetPassword(array $data)
+    {
+        try {
+            $passwordResetTokens = DB::table('password_reset_tokens')->where('token', $data['token'])->first();
+            if (!isset($passwordResetTokens)) throw new UserException("Token invalid");
+
+            User::where('email', $passwordResetTokens->email)->update([
+                'password' => Hash::make($data['password']),
+            ]);
         } catch (\Exception $e) {
             throw new UserException('', $e->getCode(), $e);
         }
