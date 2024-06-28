1c274b6aa28daef97a431b88688da894f3bc0ae9
andre <andrealbuquerque2001@gmail.com>
2024-06-28 12:17:03 -0300

    feat: new resource

---
+++ Api/app/Http/Resources/User2Resource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class User2Resource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            'idUser' => $this->idUser,
+            'name' => $this->name,
+        ];
+    }
+}
