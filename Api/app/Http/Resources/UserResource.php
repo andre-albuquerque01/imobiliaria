677c21e8d8677022c9ed88fa8a9519a5f679525a
andre <andrealbuquerque2001@gmail.com>
2024-06-28 12:16:29 -0300

    feat: update resource

--- Api/app/Http/Resources/UserResource.php
+++ Api/app/Http/Resources/UserResource.php
@@ -14,6 +14,11 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'idUser' => $this->idUser,
+            'name' => $this->name,
+            'email' => $this->email,
+        ];
     }
 }
