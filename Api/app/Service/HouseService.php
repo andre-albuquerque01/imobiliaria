c3b57b4d1415b98b924d7705ee8a065942b5b6e5
andre <andrealbuquerque2001@gmail.com>
2024-06-28 12:16:46 -0300

    feat: update service

--- Api/app/Service/HouseService.php
+++ Api/app/Service/HouseService.php
@@ -15,9 +15,7 @@ class HouseService
     public function index()
     {
         try {
-            $house = User::with(['house' => function ($query) {
-                $query->whereNull('deleted_at')->with('images');
-            }])->get();
+            $house = House::with('images')->with('user')->get();
             return HouseResource::collection($house);
         } catch (\Exception $e) {
             throw new HouseException('', $e->getCode(), $e);
@@ -26,7 +24,7 @@ class HouseService
 
     public function store(array $data)
     {
-        // try {
+        try {
             $house = auth()->user()->house()->create($data);
 
             $images = [
@@ -38,9 +36,9 @@ class HouseService
             ];
             Images::create($images);
             return new GeneralResource(['message' => 'success']);
-        // } catch (\Exception $e) {
-        //     throw new HouseException('', $e->getCode(), $e);
-        // }
+        } catch (\Exception $e) {
+            throw new HouseException('', $e->getCode(), $e);
+        }
     }
 
     public function show(string $id)
