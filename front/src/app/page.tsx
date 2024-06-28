fdabc8720ff0e670e8b8b3b20d3f99f38bf54372
andre <andrealbuquerque2001@gmail.com>
2024-06-28 12:21:42 -0300

    feat: update page

---
+++ front/src/app/page.tsx
@@ -0,0 +1,50 @@
+import { Card } from '@/components/cardHome/card'
+
+export default function Home() {
+  return (
+    <div className="mt-10 flex flex-col gap-3">
+      <Card
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5OGbIcXNwv2QdjS5j3fnLL4nWkAg85Gfgjw&usqp=CAU"
+        id="1"
+        location="Casa bela"
+        price={500}
+        title="Jesus is good"
+      />
+      <Card
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5OGbIcXNwv2QdjS5j3fnLL4nWkAg85Gfgjw&usqp=CAU"
+        id="1"
+        location="Casa bela"
+        price={500}
+        title="Jesus is good"
+      />
+      <Card
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5OGbIcXNwv2QdjS5j3fnLL4nWkAg85Gfgjw&usqp=CAU"
+        id="1"
+        location="Casa bela"
+        price={500}
+        title="Jesus is good"
+      />
+      <Card
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5OGbIcXNwv2QdjS5j3fnLL4nWkAg85Gfgjw&usqp=CAU"
+        id="1"
+        location="Casa bela"
+        price={500}
+        title="Jesus is good"
+      />
+      <Card
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5OGbIcXNwv2QdjS5j3fnLL4nWkAg85Gfgjw&usqp=CAU"
+        id="1"
+        location="Casa bela"
+        price={500}
+        title="Jesus is good"
+      />
+      <Card
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5OGbIcXNwv2QdjS5j3fnLL4nWkAg85Gfgjw&usqp=CAU"
+        id="1"
+        location="Casa bela"
+        price={500}
+        title="Jesus is good"
+      />
+    </div>
+  )
+}
