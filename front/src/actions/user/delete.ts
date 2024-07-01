88b82af7f28801862eaae45a428e7975f86950bb
andre <andrealbuquerque2001@gmail.com>
2024-07-01 12:55:12 -0300

    feat: create new action

---
+++ front/src/actions/user/delete.ts
@@ -0,0 +1,21 @@
+'use server'
+
+import ApiAction from '@/functions/data/apiAction'
+import { RevalidateTag } from '@/functions/data/revalidateTag'
+import apiError from '@/functions/error/apiErro'
+import { cookies } from 'next/headers'
+
+export async function DeleteUser() {
+  try {
+    await ApiAction(`/user`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + cookies().get('token')?.value,
+      },
+    })
+  } catch (error) {
+    return apiError(error)
+  }
+  RevalidateTag('user')
+}
