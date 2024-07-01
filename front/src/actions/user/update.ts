7fd5ccf3ce73923d026108fe0db2e2891941393b
andre <andrealbuquerque2001@gmail.com>
2024-07-01 12:55:28 -0300

    feat: create new action

---
+++ front/src/actions/user/update.ts
@@ -0,0 +1,24 @@
+'use server'
+
+import ApiAction from '@/functions/data/apiAction'
+import { cookies } from 'next/headers'
+
+export async function UpdateUser(reqBody: object) {
+  try {
+    const response = await ApiAction('/user', {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + cookies().get('token')?.value,
+      },
+      body: JSON.stringify(reqBody),
+    })
+
+    const data = await response.json()
+
+    return data
+  } catch (error) {
+    return 'Houver error'
+  }
+}
