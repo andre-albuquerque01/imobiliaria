f058568337965d06649ba8cd157e9574061e082d
andre <andrealbuquerque2001@gmail.com>
2024-07-02 12:30:29 -0300

    feat: update migrate

--- Api/database/migrations/0001_01_01_000000_create_users_table.php
+++ Api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->ulid('idUser')->primary();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('contact')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('is_admin')->default(false);
             $table->string('password');
