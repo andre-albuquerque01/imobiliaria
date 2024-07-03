fd9e91f593db8c5e133f3124db2b0ea25733a314
andre <andrealbuquerque2001@gmail.com>
2024-07-03 12:09:15 -0300

    feat: create new component

---
+++ front/src/components/house/delete/delete.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { DeleteHouse } from '@/actions/house/delete'
+import { FormEvent } from 'react'
+import { FiTrash } from 'react-icons/fi'
+
+export const DeleteHouseComponente = ({ id }: { id: string }) => {
+  async function handleDelete(e: FormEvent) {
+    e.preventDefault()
+    if (confirm('Deseja excluir a publicação?')) {
+      const response = await DeleteHouse(id)
+      if (response) alert('Publicação excluída com sucesso!')
+    }
+  }
+
+  return (
+    <button onClick={handleDelete} className=" hover:text-red-500">
+      <FiTrash size={20} />
+    </button>
+  )
+}
