d9dd79d126f1f337ee1ad2f5247989dfad4a00af
andre <andrealbuquerque2001@gmail.com>
2024-07-03 12:07:25 -0300

    feat: create new action

---
+++ front/src/actions/house/getHouseUser.ts
@@ -0,0 +1,33 @@
+'use server'
+
+import ApiAction from '@/functions/data/apiAction'
+import { cookies } from 'next/headers'
+
+export async function ShowHousesUser(page: number) {
+  try {
+    const response = await ApiAction(`/housesUser?page=${page}`, {
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + cookies().get('token')?.value,
+      },
+      next: {
+        revalidate: 1,
+        tags: ['house'],
+      },
+    })
+
+    const datas = await response.json()
+
+    if (!datas.meta || typeof datas.meta.last_page === 'undefined') {
+      throw new Error('Estrutura de dados inesperada.')
+    }
+
+    const countPage = datas.meta.last_page
+    const data = datas.data
+
+    return { data, countPage }
+  } catch (err) {
+    console.log(err)
+    return null
+  }
+}
