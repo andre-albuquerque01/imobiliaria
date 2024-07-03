57ee0f7b83a400aa1041669a9f121516f103b49f
andre <andrealbuquerque2001@gmail.com>
2024-07-03 12:07:40 -0300

    feat: create new page

---
+++ front/src/app/house/insert/page.tsx
@@ -0,0 +1,17 @@
+import { InsertHouseComponent } from '@/components/house/insert/InsertHouseComponent'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s',
+    default: 'Insert anúncio',
+  },
+}
+
+export default function InsertHouse() {
+  return (
+    <div className="flex justify-center items-center min-h-screen ">
+      <InsertHouseComponent />
+    </div>
+  )
+}
