3fafc941fda77312e71c802ed8b7e73e5b562c9a
andre <andrealbuquerque2001@gmail.com>
2024-07-04 11:07:29 -0300

    feat: update actions

--- front/src/actions/house/getOne.ts
+++ front/src/actions/house/getOne.ts
@@ -1,14 +1,12 @@
 'use server'
 
 import ApiAction from '@/functions/data/apiAction'
-import { cookies } from 'next/headers'
 
 export async function ShowOneHouses(id: string) {
   try {
     const response = await ApiAction(`/house/${id}`, {
       headers: {
         Accept: 'application/json',
-        Authorization: 'Bearer' + cookies().get('token')?.value,
       },
       next: {
         revalidate: 1,
