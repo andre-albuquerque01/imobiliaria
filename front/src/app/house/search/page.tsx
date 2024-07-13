f57b6d3264b2d9a4a5d23a9a6b9712067f3dd741
Andre <andrealbuquerque2001@gmail.com>
2024-07-13 10:00:00 -0300

    feat: create page

---
+++ front/src/app/house/search/page.tsx
@@ -0,0 +1,44 @@
+import { ShowHousesSearch } from '@/actions/house/search'
+import { Card } from '@/components/cardHome/card'
+import SearchForm from '@/components/house/search/searchQuery'
+interface SearchParamsProps {
+  searchParams: {
+    q: string
+    page: number
+  }
+}
+
+type requestType = {
+  data: []
+  countPage: number
+}
+export default async function Search({ searchParams }: SearchParamsProps) {
+  let { page } = searchParams || 1
+  if (page === undefined) page = 1
+
+  const { q: queryQ } = searchParams
+
+  const dt = (await ShowHousesSearch(queryQ, page)) as requestType
+  const data = dt.data
+  const count = dt.countPage
+
+  return (
+    <div className="flex flex-col gap-3 my-3">
+      <div className="md:w-[800px] max-md:w-80 mx-auto">
+        <SearchForm />
+        <h1 className="font-semibold text-sm mt-2">Pesquisado por: {queryQ}</h1>
+      </div>
+      {data && data.length > 0 ? (
+        <Card
+          countPage={count}
+          query={page}
+          data={data}
+          letter="q"
+          title={queryQ}
+        />
+      ) : (
+        <p>Não encontramos anúncios desejados!</p>
+      )}
+    </div>
+  )
+}
