4c27a18a5fab850211eb1c7fc65c8d4387b89a00
andre <andrealbuquerque2001@gmail.com>
2024-07-15 13:55:29 -0300

    feat: update page

--- front/src/app/user/recover/sendEmail/page.tsx
+++ front/src/app/user/recover/sendEmail/page.tsx
@@ -2,7 +2,7 @@ import { SendEmailComponent } from '@/components/user/recover/sendEmail'
 
 export default function SendEmail() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen mt-[-80px]">
       <SendEmailComponent />
     </div>
   )
