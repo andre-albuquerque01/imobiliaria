33a8e97b9e54ed3d2dd9e9a7ec2b5364a342d3a6
andre <andrealbuquerque2001@gmail.com>
2024-07-15 13:55:14 -0300

    feat: update component

--- front/src/components/cardHome/card.tsx
+++ front/src/components/cardHome/card.tsx
@@ -3,15 +3,20 @@ import Link from 'next/link'
 import { IoLocationOutline } from 'react-icons/io5'
 import LinkPagination from '../pagination/pagination'
 import { HouseInterface } from '@/interfaces/all'
+import LinkPaginationSearch from '../pagination/paginationSearch'
 
 export const Card = ({
   data,
   query,
   countPage,
+  letter,
+  title,
 }: {
   data: HouseInterface[]
   query: number
   countPage: number
+  letter: string
+  title?: string
 }) => {
   return (
     <>
@@ -19,7 +24,7 @@ export const Card = ({
         data.length > 0 &&
         data.map((house) => (
           <div
-            className="md:w-[800px] max-md:w-80 border border-zinc-500 md:h-40 mx-auto transform duration-200 hover:text-zinc-400"
+            className="md:w-[800px] max-md:w-80 border md:h-40 mx-auto max-md:max-h-[120px] rounded-lg transform duration-200 hover:text-zinc-400"
             key={house.idHouse}
           >
             <Link
@@ -34,14 +39,14 @@ export const Card = ({
                 width={150}
                 height={150}
                 alt="Imagem do anúncio"
-                className="object-contain max-h-[160px] max-w-[160px] mx-4"
+                className="object-contain max-h-40 w-[30%] max-w-40 max-md:max-h-[90px] mx-4"
               />
-              <div className="flex justify-between w-full max-md:flex-col">
+              <div className="flex justify-between w-[70%] max-md:flex-col">
                 <div className="flex flex-col justify-between">
                   <h1>{house.title}</h1>
-                  <p className="text-sm opacity-80 flex items-center">
+                  <p className="text-sm opacity-80 flex items-center w-full text-wrap ">
                     <IoLocationOutline size={18} />
-                    {house.address}
+                    <span className="truncate w-[80%]">{house.address}</span>
                   </p>
                 </div>
                 <div className="">
@@ -51,7 +56,16 @@ export const Card = ({
             </Link>
           </div>
         ))}
-      <LinkPagination query={query} countPage={countPage} />
+      {letter === 'p' ? (
+        <LinkPagination query={query} countPage={countPage} />
+      ) : (
+        <LinkPaginationSearch
+          query={query}
+          countPage={countPage}
+          path={title}
+          letter="q"
+        />
+      )}
     </>
   )
 }
