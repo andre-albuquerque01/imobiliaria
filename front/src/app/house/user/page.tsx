8ee9b532437df23952cf58b8c710b199a1aa4943
andre <andrealbuquerque2001@gmail.com>
2024-07-16 11:26:31 -0300

    feat: update page

--- front/src/app/house/user/page.tsx
+++ front/src/app/house/user/page.tsx
@@ -1,5 +1,8 @@
 import { ShowHousesUser } from '@/actions/house/getHouseUser'
 import { CardHouseUser } from '@/components/house/cardHouseUser/card'
+import Link from 'next/link'
+import { BiPlusCircle } from 'react-icons/bi'
+import { BsPersonWorkspace } from 'react-icons/bs'
 
 interface SearchParamsProps {
   searchParams: {
@@ -20,8 +23,20 @@ export default async function HouseUser({ searchParams }: SearchParamsProps) {
   const count = dt.countPage
 
   return (
-    <div className="mt-10 flex flex-col gap-3">
-      <h1 className="font-bold">Meus anúncios</h1>
+    <div className="mt-2 flex flex-col gap-3">
+      <div className="md:w-[800px] max-md:w-80 mx-auto space-y-3">
+        <Link
+          href="/house/insert"
+          className="flex items-center text-blue-500 gap-2 w-40 hover:underline"
+        >
+          <BiPlusCircle size={23} />
+          Adicionar anúncio
+        </Link>
+        <h1 className="flex items-center gap-2">
+          <BsPersonWorkspace size={23} />
+          Meus anúncios
+        </h1>
+      </div>
       {data.length > 0 ? (
         <CardHouseUser countPage={count} query={page} data={data} />
       ) : (
