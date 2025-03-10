1f819e18acaebbe19c98094a0e9dfb48885ccb9f
andre <andrealbuquerque2001@gmail.com>
2025-03-10 11:46:18 -0300

    feat: update

--- Api/app/Http/Requests/HouseRequest.php
+++ Api/app/Http/Requests/HouseRequest.php
@@ -24,13 +24,11 @@ class HouseRequest extends FormRequest
         $rules = [
             "title" => ["required", "string", "min:4", "max:255"],
             "description" => ["required", "string", "min:4", "max:255"],
-            "rooms" => ["nullable", "string", "min:1", "max:255"],
+            "rooms" => ["nullable", "string", "min:1", "max:20"],
             "value" => ["required", "string", "min:2", "max:255"],
             "address" => ["required", "string", "min:2", "max:255"],
-            "imageOne" => ["required", "string", "min:2"],
-            "imageTwo" => ["nullable", "string", "min:2"],
-            "imageThree" => ["nullable", "string", "min:2"],
-            "imageFour" => ["nullable", "string", "min:2"],
+            "image" => ["required", "array", "min:1"], 
+            "image.*" => ["required", "string", "url"], 
         ];
 
         if ($this->method() === "PUT") {
@@ -38,8 +36,39 @@ class HouseRequest extends FormRequest
             $rules["description"] = ["nullable", "string", "min:4", "max:255"];
             $rules["value"] = ["nullable", "string", "min:2", "max:255"];
             $rules["address"] = ["nullable", "string", "min:2", "max:255"];
-            $rules["imageOne"] = ["nullable", "string", "min:2"];
+            $rules["image"] = ["nullable", "array"];
+            $rules["image.*"] = ["nullable", "string", "url"];
         }
+
         return $rules;
+    }
+
+
+    public function messages()
+    {
+        return [
+            "token.required" => "O token é obrigatório.",
+
+            "title.required" => "O titulo é obrigatório.",
+            "title.max" => "O titulo não pode ter mais de 255 caracteres.",
+            "title.min" => "O titulo deve ter pelo menos 4 caracteres.",
+
+            "description.required" => "O descrição é obrigatório.",
+            "description.max" => "O descrição não pode ter mais de 255 caracteres.",
+            "description.min" => "O descrição deve ter pelo menos 4 caracteres.",
+
+            "value.required" => "O valor é obrigatório.",
+            "value.max" => "O valor não pode ter mais de 255 caracteres.",
+            "value.min" => "O valor deve ter pelo menos 2 caracteres.",
+
+            "rooms.max" => "O quarto não pode ter mais de 20 caracteres.",
+            "rooms.min" => "O quarto deve ter pelo menos 1 caracteres.",
+
+            "address.max" => "O endereço não pode ter mais de 20 caracteres.",
+            "address.min" => "O endereço deve ter pelo menos 1 caracteres.",
+
+            "image.max" => "O endereço não pode ter mais de 20 caracteres.",
+            "image.min" => "O endereço deve ter pelo menos 1 caracteres.",
+        ];
     }
 }
