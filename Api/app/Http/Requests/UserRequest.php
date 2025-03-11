7876687a267fca0e0b756eb3eaad1d7dbf2093c9
andre <andrealbuquerque2001@gmail.com>
2025-03-11 09:58:29 -0300

    feat: update

--- Api/app/Http/Requests/UserRequest.php
+++ Api/app/Http/Requests/UserRequest.php
@@ -26,6 +26,7 @@ class UserRequest extends FormRequest
         $rules = [
             "name" => ["required", "string", "min:4", "max:255"],
             "contact" => ["required", "string", "min:8", "max:11"],
+            "term_aceite" => ["required"],
             "email" => [
                 "required",
                 "email",
@@ -73,8 +74,44 @@ class UserRequest extends FormRequest
                 ->uncompromised(),
             ];
             $rules["password_confirmation"] = ["nullable"];
+            $rules["term_aceite"] = ["nullable"];
             $rules["contact"] = ["nullable", "string", "min:8", "max:11"];
         }
         return $rules;
+    }
+
+    public function messages()
+    {
+        return [
+            "name.required" => "O nome é obrigatório.",
+            "name.min" => "O nome deve ter pelo menos 3 caracteres.",
+            "name.max" => "O nome não pode ter mais de 120 caracteres.",
+            "name.regex" => "O nome contém caracteres inválidos.",
+            
+            "contact.required" => "O contato é obrigatório.",
+            "contact.min" => "O nome deve ter pelo menos 8 caracteres.",
+            "contact.max" => "O nome não pode ter mais de 11 caracteres.",
+            
+            "term_aceite.required" => "É necessário aceitar os termos.",
+            "term_aceite.regex" => "O campo de aceite contém caracteres inválidos.",
+
+            "email.required" => "O e-mail é obrigatório.",
+            "email.email" => "O e-mail informado não é válido.",
+            "email.max" => "O e-mail não pode ter mais de 255 caracteres.",
+            "email.min" => "O e-mail deve ter pelo menos 2 caracteres.",
+            "email.unique" => "Este e-mail já está cadastrado.",
+
+            "password.required" => "A senha é obrigatória.",
+            "password.confirmed" => "A confirmação da senha não corresponde.",
+            "password.min" => "A senha deve ter pelo menos 8 caracteres.",
+            "password.mixedCase" => "A senha deve conter pelo menos uma letra maiúscula e uma minúscula.",
+            "password.letters" => "A senha deve conter pelo menos uma letra.",
+            "password.numbers" => "A senha deve conter pelo menos um número.",
+            "password.symbols" => "A senha deve conter pelo menos um símbolo.",
+            "password.uncompromised" => "A senha escolhida já apareceu em vazamentos de dados. Escolha outra senha.",
+
+            "password_confirmation.required" => "A confirmação da senha é obrigatória.",
+            "password_confirmation.min" => "A confirmação da senha deve ter pelo menos 8 caracteres.",
+        ];
     }
 }
