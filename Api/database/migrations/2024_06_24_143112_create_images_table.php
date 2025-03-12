7e238c2954a0d20fa1c7cc7392f924b704090769
andre <andrealbuquerque2001@gmail.com>
2025-03-12 10:22:10 -0300

    feat: update

--- Api/database/migrations/2024_06_24_143112_create_images_table.php
+++ Api/database/migrations/2024_06_24_143112_create_images_table.php
@@ -13,11 +13,7 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->ulid('idImage')->primary();
-            $table->longText('imageOne');
-            $table->longText('imageTwo');
-            $table->longText('imageThree');
-            $table->longText('imageFour');
-            $table->index('house_id');
+            $table->string('image');
             $table->foreignUlid('house_id')->constrained("houses", 'idHouse')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
