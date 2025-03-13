b3192dbb699ebff06b63438e7c7eb7c5af8ade4d
andre <andrealbuquerque2001@gmail.com>
2025-03-13 10:50:39 -0300

    feat: create

---
+++ Api/database/factories/HouseFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\House>
+ */
+class HouseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->title(),
+            'description' => fake()->text(50),
+            'rooms' => fake()->randomNumber(2),
+            'value' => fake()->randomNumber(3),
+            'address' => fake()->address(),
+        ];
+    }
+}
