401397079bac5ebeff8340aecb2308f15c68f7c8
andre <andrealbuquerque2001@gmail.com>
2025-03-17 11:38:50 -0300

    feat: update

--- front/src/actions/user/update.ts
+++ front/src/actions/user/update.ts
@@ -1,11 +1,12 @@
 'use server'
 
 import ApiAction from '@/functions/data/apiAction'
+import { UserRequestWithReturnError } from '@/functions/error/user-request'
 import { cookies } from 'next/headers'
 
 export async function UpdateUser(reqBody: object) {
   try {
-    const response = await ApiAction('/user', {
+    const response = await ApiAction('/user/update', {
       method: 'PUT',
       headers: {
         Accept: 'application/json',
@@ -16,7 +17,7 @@ export async function UpdateUser(reqBody: object) {
     })
 
     const data = await response.json()
-    if (!response.ok) return data
+    if (!response.ok) return UserRequestWithReturnError(data.message)
     return true
   } catch (error) {
     return 'Houver error'
