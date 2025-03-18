8ad72c823e45b6d2c24edaae98cda23916a0cd63
andre <andrealbuquerque2001@gmail.com>
2025-03-18 10:42:24 -0300

    feat: update

--- front/src/app/house/[id]/page.tsx
+++ front/src/app/house/[id]/page.tsx
@@ -1,8 +1,8 @@
 import { ShowOneHouses } from '@/actions/house/getOne'
 import User from '@/actions/user/show'
+import Carousel from '@/components/house/carousel'
 import { GotBack } from '@/components/others/gotBack'
 import { HouseInterface, UserInterface } from '@/interfaces/all'
-import Image from 'next/image'
 import Link from 'next/link'
 import { Suspense } from 'react'
 import { HiOutlinePencil } from 'react-icons/hi'
@@ -13,74 +13,38 @@ export default async function HouseOne({ params }: { params: { id: string } }) {
 
   return (
     <Suspense>
-      <div className="px-4">
+      <div className="px-6 py-4 max-w-5xl mx-auto bg-white">
         <GotBack />
-        <div className="flex flex-col ">
-          <div>
-            <div className="mt-5 space-y-1 max-w-full">
-              {data.image.map((images, key) => (
-                <div key={key} className="flex flex-wrap gap-5 items-center">
-                  {images.imageOne && (
-                    <Image
-                      src={images.imageOne}
-                      width={320}
-                      height={320}
-                      alt="Imagem"
-                      className="max-w-80 min-w-20 max-h-80 min-h-10 object-contain"
-                    />
-                  )}
-                  {images.imageTwo && (
-                    <Image
-                      src={images.imageTwo}
-                      width={320}
-                      height={320}
-                      alt="Imagem"
-                      className="max-w-80 min-w-20 max-h-80 min-h-10 object-contain"
-                    />
-                  )}
-                  {images.imageThree && (
-                    <Image
-                      src={images.imageThree}
-                      width={320}
-                      height={320}
-                      alt="Imagem"
-                      className="max-w-80 min-w-20 max-h-80 min-h-10 object-contain"
-                    />
-                  )}
-                  {images.imageFour && (
-                    <Image
-                      src={images.imageFour}
-                      width={320}
-                      height={320}
-                      alt="Imagem"
-                      className="max-w-80 min-w-20 max-h-80 min-h-10 object-contain"
-                    />
-                  )}
-                </div>
-              ))}
-            </div>
-            <div className="my-5">
-              <p className="font-medium text-lg">{data.title}</p>
-              <p className="font-normal text-md">
-                Descrição: {data.description}
-              </p>
-              <p className="font-normal text-md">Quartos: {data.rooms}</p>
-              <p className="font-normal text-md">Valor: {data.value}</p>
-              <p className="font-normal text-md">Endereço: {data.address}</p>
-              <p className="font-normal text-md">
-                Contato: {data.user.contact}
-              </p>
-            </div>
+        <div className="flex flex-col md:flex-row gap-8 mt-5">
+          <div className="w-full md:w-2/3">
+            <Carousel images={data.image.map((img) => img.image)} />
           </div>
-          {data.user && data.user.idUser === user?.idUser && (
-            <Link
-              href={`/house/update/${data.idHouse}`}
-              className="text-blue-500 mt-5 flex items-center gap-1"
-            >
-              <HiOutlinePencil className="w-5 h-5" />
-              Alterar publicação
-            </Link>
-          )}
+
+          {/* Informações ao lado no Desktop */}
+          <div className="w-full md:w-1/3 flex flex-col justify-center space-y-3">
+            <h1 className="text-2xl font-bold text-gray-800">{data.title}</h1>
+            <p className="text-gray-600 break-words">{data.description}</p>
+            <p className="text-gray-700 font-medium">
+              🛏 Quartos: {data.rooms}
+            </p>
+            <p className="text-gray-700 font-medium">💰 Valor: {data.value}</p>
+            <p className="text-gray-700 font-medium">
+              📍 Endereço: {data.address}
+            </p>
+            <p className="text-gray-700 font-medium">
+              📞 Contato: {data.user?.contact}
+            </p>
+
+            {data.user && data.user.idUser === user?.idUser && (
+              <Link
+                href={`/house/update/${data.idHouse}`}
+                className="mt-4 inline-flex items-center gap-2 text-blue-600 hover:text-blue-800 transition"
+              >
+                <HiOutlinePencil className="w-5 h-5" />
+                Alterar publicação
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </Suspense>
