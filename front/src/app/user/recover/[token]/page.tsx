af997e10066166b82af01f3c7d299f6ae114923d
andre <andrealbuquerque2001@gmail.com>
2025-03-18 10:43:05 -0300

    feat: update

--- front/src/app/user/recover/[token]/page.tsx
+++ front/src/app/user/recover/[token]/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation'
 export default function SendEmail({ params }: { params: { token: string } }) {
   if (!params) redirect('/')
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-[calc(100vh-80px)]">
       <UpdatePasswordRecoverComponent token={params.token} />
     </div>
   )
