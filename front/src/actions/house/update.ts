8172626dfa3aeaeab51294a117278d2816abfecb
andre <andrealbuquerque2001@gmail.com>
2025-03-19 11:57:55 -0300

    feat: update

--- front/src/actions/house/update.ts
+++ front/src/actions/house/update.ts
@@ -5,8 +5,26 @@ import { RevalidateTag } from '@/functions/data/revalidateTag'
 import { HouseRequestWithReturnError } from '@/functions/error/house-request'
 import { cookies } from 'next/headers'
 
-export async function UpdateHouse(reqBody: object, id: string) {
+export type requestType = {
+  title: string
+  description: string
+  value: number | string
+  address: string
+  rooms: number | string
+  image: string[]
+}
+
+export async function UpdateHouse(request: requestType, id: string) {
   try {
+    if (
+      !request.title ||
+      !request.description ||
+      !request.address ||
+      !request.value ||
+      request.image.length === 0
+    ) {
+      return 'Preenchas os dados!'
+    }
     const response = await ApiAction(`/house/${id}`, {
       method: 'PUT',
       headers: {
@@ -14,7 +32,7 @@ export async function UpdateHouse(reqBody: object, id: string) {
         'Content-Type': 'application/json',
         Authorization: 'Bearer ' + cookies().get('token')?.value,
       },
-      body: JSON.stringify(reqBody),
+      body: JSON.stringify(request),
     })
 
     const data = await response.json()
@@ -27,6 +45,7 @@ export async function UpdateHouse(reqBody: object, id: string) {
       RevalidateTag('house')
       return 'true'
     }
+    return ''
   } catch (error) {
     return 'Houver error'
   }
