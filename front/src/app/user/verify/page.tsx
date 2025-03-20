5d1229b8eacc2234df496d8852a81aae2805a144
andre <andrealbuquerque2001@gmail.com>
2025-03-20 11:02:12 -0300

    feat: update

--- front/src/app/user/verify/page.tsx
+++ front/src/app/user/verify/page.tsx
@@ -2,7 +2,7 @@ import { SendEmailVerifyComponent } from '@/components/user/verify/SendEmailVeri
 
 export default function SendEmailVerify() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-[calc(100vh-80px)]">
       <SendEmailVerifyComponent />
     </div>
   )
