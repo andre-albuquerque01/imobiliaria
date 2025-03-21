2441b27469a0e5cd1f965819fa45113c7a37834d
andre <andrealbuquerque2001@gmail.com>
2025-03-21 10:37:38 -0300

    feat: update

--- front/src/app/error.tsx
+++ front/src/app/error.tsx
@@ -13,7 +13,7 @@ export default function Error({
   }, [error])
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen gap-3 mt-[-80px]">
+    <div className="flex flex-col justify-center items-center gap-3 h-[calc(100vh-80px)]">
       <h1 className="text-black text-2xl">
         Olá! Desculpe, infelizmente aconteceu algo não esperado.
       </h1>
