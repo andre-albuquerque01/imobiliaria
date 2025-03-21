dc95acfb78c3ba28fd13aadad8d483f90b9dd7d1
andre <andrealbuquerque2001@gmail.com>
2025-03-21 10:37:44 -0300

    feat: update

--- front/src/app/loading.tsx
+++ front/src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen mt-[-80px]">
+    <div className="flex flex-col items-center justify-center h-[calc(100vh-80px)]">
       <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-cyan-500"></div>
       <p className="text-white">Carregando...</p>
     </div>
